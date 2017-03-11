0942f3a5fef100a2dbff83eb1b17ec3cb8e156bf
Zach Zador <zazador@gmail.com>
2017-03-11 15:44:09 -0600

    gitignore

---
+++ assets/config.js
@@ -0,0 +1,7 @@
+  var config = {
+  	apiKey: "AIzaSyBWT9r__oUEh2rr2F1af7H5z8Mukcivyvg",
+  	authDomain: "rps-multiplayer-ee834.firebaseapp.com",
+  	databaseURL: "https://rps-multiplayer-ee834.firebaseio.com",
+  	storageBucket: "rps-multiplayer-ee834.appspot.com",
+  	messagingSenderId: "906222852635"
+  };
